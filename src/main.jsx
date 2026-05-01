5739bd3d6c77e1359f57d492870b5cc929770016
Burhoniddin Bahodirzoda <burhoniddinbahodirzoda@MacBook-Air-Burhoniddin.local>
2026-05-01 12:11:42 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
-import MenuPage from "./Menupage.jsx"
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
